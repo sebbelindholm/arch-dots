b0c97e3d2c99bc6c53bff1b27a50a8ce57f10518
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2024-03-22 17:52:45 +0100

    More cleanup.

--- Scripts/kittypadding.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env sh
-
-kitty @ set-spacing padding = 0
-nvim $*
-kitty @ set-spacing padding = default
--- Scripts/saveDconf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-dconf dump / | dconf2nix > dconf.nix
-rm -rf /home/sebastian/.setup/configs/dconf/dconf.nix
-mv dconf.nix /home/sebastian/.setup/configs/dconf/dconf.nix 
