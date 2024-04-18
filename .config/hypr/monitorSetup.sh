8f8c7502551f16a0f6cdc9e47b0c87fe894cd89f
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2024-04-18 17:56:50 +0200

    Update.

--- .config/hypr/monitorSetup.sh
+++ .config/hypr/monitorSetup.sh
@@ -10,9 +10,5 @@ case $1 in
         hyprctl keyword monitor DP-1,5120x1440@240,0x0,1,bitdepth,10,vrr,2
         hyprctl keyword monitor DP-3,disable
         ;;
-    3)
-        hyprctl keyword monitor DP-1,3360x1440@120,0x0,1,bitdepth,10,vrr,2
-        hyprctl keyword monitor DP-3,1760x1440@120,0x0,1,bitdepth,10,vrr,2
-        ;;
     esac
 
