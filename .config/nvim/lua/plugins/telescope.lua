f5b845041665fb4782f010d20943434e4c9d75f0
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2024-06-06 23:25:47 +0200

    Update.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -8,15 +8,43 @@ return {
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
             vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
----            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-            vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files hidden=true <CR>")
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+--            vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files hidden=true <CR>")
         end,
+    },
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make'
     },
     {
         "nvim-telescope/telescope-ui-select.nvim",
 
         config = function()
             require("telescope").setup({
+                fzf = {
+                    fuzzy = true,
+                        override_generic_sorter = true,
+                        override_file_sorter = true,
+                        case_mode = "smart_case",
+                },
+                defaults = {
+                    vimgrep_arguments = {
+                        'rg',
+                        '--color=never',
+                        '--no-heading',
+                        '--with-filename',
+                        '--line-number',
+                        '--column',
+                        '--hidden',
+                        '--smart-case',
+                        '--glob "!.git"',
+                    },
+            },
+            pickers = {
+                    find_files = {
+                        find_command = {"rg", "--files", "--hidden", "--glob", "!**/.git/*", "--glob", "!**/.cargo/*", "--glob", "!**/.java/*", "--glob", "!**/.cache/*", "--glob", "!**/.rustup/*", "--glob", "!**/.npm/*", "--glob", "!**/go/*", "--glob", "!**/Steam/*"},
+                    },
+                },
                 extensions = {
                     ["ui-select"] = {
                         require("telescope.themes").get_dropdown({}),
@@ -24,6 +52,7 @@ return {
                 },
             })
             require("telescope").load_extension("ui-select")
+            require("telescope").load_extension("fzf")
         end,
     },
-} 
+}
